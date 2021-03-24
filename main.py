19038587ee91fd7488f7b7b65c7f34d204e39de6
ShopaidePeople <81110982+ShopaidePeople@users.noreply.github.com>
2021-03-24 12:50:04 +0530

    Update main.py

--- main.py
+++ main.py
@@ -29,6 +29,7 @@ def speechToTextFunc():
     r = sr.Recognizer()
     audio_file=None
     audio_file = request.files['file'].filename
+    print(request.files['file'], " ", audio_file)
     print("file recieved".format(audio_file))
     if(audio_file==None):
         return flask.jsonify({"result":"Sorry"})
