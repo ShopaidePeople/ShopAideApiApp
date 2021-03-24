0ba3fd32f8f7ab7cc5463d1e5facee48fe3c021c
ShopaidePeople <81110982+ShopaidePeople@users.noreply.github.com>
2021-03-24 12:52:58 +0530

    Update main.py

--- main.py
+++ main.py
@@ -28,7 +28,7 @@ def speechToTextFunc():
     text="hello"
     r = sr.Recognizer()
     audio_file=None
-    audio_file = request.files['file'].filename
+    audio_file = request.files['file']
     print(request.files['file'], " ", audio_file)
     print("file recieved".format(audio_file))
     if(audio_file==None):
