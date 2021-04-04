cc8065fa8e0f8ee83c4a6290fa13b3fe35106b7b
ShopaidePeople <81110982+ShopaidePeople@users.noreply.github.com>
2021-04-05 02:35:36 +0530

    Update main.py

--- main.py
+++ main.py
@@ -223,6 +223,8 @@ def getRankProductsFunc():
             i[val]['price'] = i[val]['price'].replace(',', '')
             if(int(i[val]['price'])>int(max_price)):
                 max_price = int(i[val]['price'])
+            if(i[val]['deliveryFee']=="free"):
+                i[val]['deliveryFee'] =0
             if(int(i[val]['deliveryFee'])>int(max_deliveryFee)):
                 max_deliveryFee = int(i[val]['deliveryFee'])
             
