64c57075bb4a03fcb2e74db3c4d5b7d5b0a8c1e3
ShopaidePeople <81110982+ShopaidePeople@users.noreply.github.com>
2021-04-05 10:22:51 +0530

    Update main.py

--- main.py
+++ main.py
@@ -148,6 +148,7 @@ def featureIdentificationFunction():
 @app.route('/getProducts')
 def getProductsFunction():
     uid = request.args.get('uid')
+    uid=""
     features_collection = mongo.db[str(uid)+'features'].find()
 
     uid = str(uid)+'products'
@@ -174,7 +175,7 @@ def getProductsFunction():
                 if(kiv not in i.keys()):
                     absent_feature+=1
                     continue
-                if(i[kiv]==val[kiv]):
+                if(i[kiv].lower().strip()==val[kiv].lower().strip()):
                     present_feature+=1
                 else:
                     absent_feature+=1
@@ -184,13 +185,15 @@ def getProductsFunction():
             if(flag==False):
                 break
         if(present_feature == features_count):
-            k={}
-            k[str(index_var)] = i
-            result_data[str(index_var)] = i
-            index_var+=1
+            #k={}
+            #k[str(index_var)] = i
+            #result_data[str(index_var)] = i
+            collections = mongo.db[uid]
+            collections.insert_one(i)
+            #index_var+=1
 
-    collections = mongo.db[uid]
-    collections.insert_one(result_data)
+    #collections = mongo.db[uid]
+    #collections.insert_one(result_data)
 
     
     f.close()
@@ -210,111 +213,128 @@ def updateNerModelFunc():
 def getRankProductsFunc():
 
     uid = request.args.get('uid')
+    uid = ""
     uid = str(uid)+'products'
 
     max_price = -999999
     max_deliveryFee = -999999
     collections = mongo.db[uid].find() 
 
+    results_dict = {}
+    results_dict['products'] = []
+
     for i in collections:
-        for val in i:
+        #if(val=='_id'):
+        #        continue
+        i['price'] = i['price'].replace(',', '')
+        if(int(i['price'])>int(max_price)):
+            max_price = int(i['price'])
+        if(i['deliveryFee']=="free"):
+            i['deliveryFee'] =0
+        if(int(i['deliveryFee'])>int(max_deliveryFee)):
+            max_deliveryFee = int(i['deliveryFee'])
+        '''for val in i:
             if(val=='_id'):
                 continue
-            #i[val]['price'] = i[val]['price'].replace(',', '')
+            i[val]['price'] = i[val]['price'].replace(',', '')
             if(int(i[val]['price'])>int(max_price)):
                 max_price = int(i[val]['price'])
-            #if(i[val]['deliveryFee']=="free"):
-            #    i[val]['deliveryFee'] =0
+            if(i[val]['deliveryFee']=="free"):
+                i[val]['deliveryFee'] =0
             if(int(i[val]['deliveryFee'])>int(max_deliveryFee)):
-                max_deliveryFee = int(i[val]['deliveryFee'])
+                max_deliveryFee = int(i[val]['deliveryFee'])'''
             
     collections = mongo.db[uid].find()
 
     for i in collections:
-        for val in i:
-            if(val=='_id'):
-                continue
-            users = int(i[val]['totalNoofRating'])
-            if(i[val]['company']=='amazon'):
-                star1 = int(i[val]['1star'])
-                stars1 = (int(star1)/100)*users
-                star2 = int(i[val]['2star'])
-                stars2 = (int(star2)/100)*users
-                star3 = int(i[val]['3star'])
-                stars3 = (int(star3)/100)*users
-                star4 = int(i[val]['4star'])
-                stars4 = (int(star4)/100)*users
-                star5 = int(i[val]['5star'])
-                stars5 = (int(star5)/100)*users
-                ratings_ratio = 0.0
-            else:
-                stars1 = int(i[val]['1star'])
-                stars2 = int(i[val]['2star'])
-                stars3 = int(i[val]['3star'])
-                stars4 = int(i[val]['4star'])
-                stars5 = int(i[val]['5star'])
-                ratings_ratio =0.0
+        #for val in i:
+        #    if(val=='_id'):
+        #        continue
+        users = int(i['totalNoofRating'])
+        if(i['company']=='amazon'):
+            star1 = int(i['1star'])
+            stars1 = (int(star1)/100)*users
+            star2 = int(i['2star'])
+            stars2 = (int(star2)/100)*users
+            star3 = int(i['3star'])
+            stars3 = (int(star3)/100)*users
+            star4 = int(i['4star'])
+            stars4 = (int(star4)/100)*users
+            star5 = int(i['5star'])
+            stars5 = (int(star5)/100)*users
+            ratings_ratio = 0.0
+        else:
+            stars1 = int(i['1star'])
+            stars2 = int(i['2star'])
+            stars3 = int(i['3star'])
+            stars4 = int(i['4star'])
+            stars5 = int(i['5star'])
+            ratings_ratio =0.0
                     
-            if(users!=0):
-                ratings_ratio+= ((stars1+stars2+stars3)/(stars1+stars2+stars3+stars4+stars5))*0.175
-                ratings_ratio += ((stars1+stars2+stars3+stars4+stars5)/(stars4+stars5))*0.175
-            else:
-                ratings_ratio+=1.0
-                ratings_ratio += 1.0
+        if(users!=0):
+            ratings_ratio+= ((stars1+stars2+stars3)/(stars1+stars2+stars3+stars4+stars5))*0.175
+            ratings_ratio += ((stars1+stars2+stars3+stars4+stars5)/(stars4+stars5))*0.175
+        else:
+            ratings_ratio+=1.0
+            ratings_ratio += 1.0
 
-
-            price_ratio = (int(i[val]['price'])/max_price)*0.25
+        i['price'] = i['price'].replace(',', '')
+        price_ratio = (int(i['price'])/max_price)*0.25
                 
-            total_reviews_rate = 0.0
-            total_review_number = int(i[val]['totalNoofRating'])
+        total_reviews_rate = 0.0
+        total_review_number = int(i['totalNoofRating'])
 
-            if(total_review_number<=500):
-                total_reviews_rate+=1
-            elif(total_review_number<=1000):
-                total_reviews_rate+=0.75
-            elif(total_review_number<=5000):
-                total_reviews_rate+=0.5
-            else:
-                total_reviews_rate+=0.25
+        if(total_review_number<=500):
+            total_reviews_rate+=1
+        elif(total_review_number<=1000):
+            total_reviews_rate+=0.75
+        elif(total_review_number<=5000):
+            total_reviews_rate+=0.5
+        else:
+            total_reviews_rate+=0.25
 
-            deliveryTimeRatings = 0
-            if(i[val]['deliveryTime']<=1):
-                deliveryTimeRatings += 0.25
-            elif(i[val]['deliveryTime']<=3):
-                deliveryTimeRatings += 0.5
-            elif(i[val]['deliveryTime']<=7):
-                deliveryTimeRatings += 0.75
-            else:
-                deliveryTimeRatings += 1.0
+        deliveryTimeRatings = 0
+        temp = int(i['deliveryTime'])
+        if(temp<=1):
+            deliveryTimeRatings += 0.25
+        elif(temp<=3):
+            deliveryTimeRatings += 0.5
+        elif(temp<=7):
+            deliveryTimeRatings += 0.75
+        else:
+            deliveryTimeRatings += 1.0
                 
+        if(i['deliveryFee']=="free"):
+            i['deliveryFee'] =0
+
+        deliveryFeeRatings = 0.0
+        if(max_deliveryFee==0):
             deliveryFeeRatings = 0.0
-            if(max_deliveryFee==0):
-                deliveryFeeRatings = 0.0
-            else:
-                deliveryFeeRatings = int(i[val]['deliveryFee'])/max_deliveryFee*100
-            #deliveryFeeRatings = int(i[val]['deliveryFee'])/max_deliveryFee*100
+        else:
+            deliveryFeeRatings = int(i['deliveryFee'])/max_deliveryFee*100
 
-            replacementRatings = 0
-            if(i[val]['replacement']=="true"):
-                replacementRatings+=0.5
-            else:
-                replacementRatings+=1.0
+        replacementRatings = 0
+        if(i['replacement']=="true"):
+            replacementRatings+=0.5
+        else:
+            replacementRatings+=1.0
 
-            ranking_pnt = ratings_ratio + price_ratio + (total_reviews_rate*0.1) + (deliveryTimeRatings*0.1) + (deliveryFeeRatings*0.1) +(replacementRatings*0.1)
+        ranking_pnt = ratings_ratio + price_ratio + (total_reviews_rate*0.1) + (deliveryTimeRatings*0.1) + (deliveryFeeRatings*0.1) +(replacementRatings*0.1)
 
+ 
+        print(ranking_pnt)
+        filterr = {'url':i['url']}
+        newvalues = {"$set" : {'ranking_points':ranking_pnt}}
+        mongo.db[uid].update_one(filterr,newvalues)
+        #i['ranking_points'] = ranking_pnt
 
-            print(ranking_pnt)
-            #filterr = {'asin':i['asin']}
-            newvalues = {"$set" : {'ranking_points':ranking_pnt}}
-            mongo.db[uid].update_one(val,newvalues)
-    
-    
-    #mongo.db[uid].find().sort("ranking_points")
-
-
-    #required --- mongo.db[unique_id].drop()
-    return '<h1>Products ranked successfully</h1>'
-
+        
+    collections = mongo.db[uid].find()
+    for i in collections:
+        results_dict['products'].append(i)
+    print("success")
+    print(results_dict)
+    return results_dict
 
 if __name__ == "__main__":
     #app.debug=True
